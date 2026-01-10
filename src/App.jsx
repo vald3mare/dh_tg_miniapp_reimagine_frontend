6fe552352daf4319f2f2d251397d4af4ddcb25d1
emelyanovvv@navigator.private <->
2026-01-10 15:04:41 +0300

    Change fetch-method

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
 
     // Отправляем сырую init data на бекенд
     fetch('https://vald3mare-dh-tg-miniapp-reimagine-backend-e40f.twc1.net/', {
-      method: 'POST',
+      method: 'GET',
       headers: {
         Authorization: `tma ${initDataRaw}`,
       },
