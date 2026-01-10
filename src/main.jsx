bd3579941bf7f518c258924c55da3ad890577c84
emelyanovvv@navigator.private <->
2026-01-10 19:04:14 +0300

    Test new NavBar

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App.jsx';
-import { UserProvider } from './context/userContext';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.jsx'
+import { UserProvider } from './context/userContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
