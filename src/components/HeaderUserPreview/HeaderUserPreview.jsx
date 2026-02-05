83e3111bc9717bcbc4788b10f3e77b434a490acf
vald3mare <work@vadimemeljanov.ru>
2026-02-05 11:27:08 +0300

    update css

--- src/components/HeaderUserPreview/HeaderUserPreview.jsx
+++ src/components/HeaderUserPreview/HeaderUserPreview.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { retrieveRawInitData } from '@tma.js/sdk';
 
 export default function HeaderUserPreview({ user }) {
   return (
     <div className='userpreview-wrapper'>
-      <img src={user.photo_url} alt="User Avatar" width={50} height={50} />
-      <p className='user-name'></p>
+      <img src={user.photo_url} alt="User Avatar" width={30} height={30} />
+      <p className='user-name'>@tgnickname</p>
     </div>
   );
 }
