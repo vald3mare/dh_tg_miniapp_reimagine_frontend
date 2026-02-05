e11c6ef300cea8b0bb4365f6656d8243355f9d24
vald3mare <work@vadimemeljanov.ru>
2026-02-05 09:01:47 +0300

    debug init data

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { AuthInitializer } from '../context/UserContext';
 
 const Profile = () => {
+  const [initData, setInitData] = useState(null);
+  
+  useEffect(() => {
+    const initDataRaw = retrieveRawInitData();
+    setInitData(initDataRaw);
+  }, []);
+  
   return (
     <div>
       <h1>Профиль пользователя</h1>
       <p>Это заглушка для страницы профиля</p>
+      <p>{initData}</p>
     </div>
   );
 };
