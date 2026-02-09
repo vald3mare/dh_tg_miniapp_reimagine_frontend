57865727037ae2396a357c84342a6a296bd906c3
vald3mare <work@vadimemeljanov.ru>
2026-02-09 21:54:16 +0300

    fix catalog component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import { Slider } from '../components/Slider/Slider';
 import Header from '../components/Header/Header';
 import SliderItem from '../components/SliderItem/SliderItem';
